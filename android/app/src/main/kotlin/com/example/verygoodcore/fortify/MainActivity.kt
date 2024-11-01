1784223ad1764c54264534316ceba144773b94b0
blehotay <ben@theappdistillery.com>
2024-11-01 15:00:55 -0400

    second commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/fortify/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.fortify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:fortify/app/app.dart';
+import 'package:fortify/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
